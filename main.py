61ec2b1765bdca264ff1eb939a28dedb79ed9c1d
Mamalikeshi <86573811+Mamalikeshi@users.noreply.github.com>
2026-08-07 04:18:38 +0330

    Connect API routes

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from backend.database import init_database
-
+from api import router
 app = FastAPI(title="Mamali Games")
-
+app.include_router(router)
 templates = Jinja2Templates(directory="templates")
 
 
