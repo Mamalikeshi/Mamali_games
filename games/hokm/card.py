fd0a45540d70a1fc12be844a5ffd9bcecf31bdaa
Mamalikeshi <86573811+Mamalikeshi@users.noreply.github.com>
2026-08-09 03:05:35 +0330

    Fix Hokm card model

--- games/hokm/card.py
+++ games/hokm/card.py
@@ -1,35 +1,44 @@
-from dataclasses import dataclass
-
-
-SUITS = ["hearts", "diamonds", "clubs", "spades"]
-
-RANKS = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-    "A",
-]
-
-
-@dataclass(frozen=True)
 class Card:
-    suit: str
-    rank: str
+    SUITS = {
+        "hearts",
+        "diamonds",
+        "clubs",
+        "spades",
+    }
+
+    RANKS = {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+        "A": 14,
+    }
+
+    def __init__(self, suit: str, rank: str):
+        if suit not in self.SUITS:
+            raise ValueError("Invalid suit.")
+
+        if rank not in self.RANKS:
+            raise ValueError("Invalid rank.")
+
+        self.suit = suit
+        self.rank = rank
+        self.rank_value = self.RANKS[rank]
 
     def to_dict(self):
         return {
             "suit": self.suit,
             "rank": self.rank,
+            "rank_value": self.rank_value,
         }
 
-    def __str__(self):
+    def __repr__(self):
         return f"{self.rank} of {self.suit}"
